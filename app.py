0c40b45d38ea56d9b68382cc6a91e22490ca7bb2
Mohamammed Faizal <mohammad.faizal@digital14.com>
2022-03-05 11:53:01 +0400

    find the length of line and change to green when below 50

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import cv2
 import time
 import numpy as np
 import handTrackingModule as htm
-
+import math
 
 wCam, hCam = 640, 480
 cap = cv2.VideoCapture(0)
@@ -14,17 +14,34 @@ cTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
-
 while True:
     success, img = cap.read()
     detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        print(lmList[4], lmList[8])
+        #print(lmList[4], lmList[8])
+
+        # Get the x,y coordinates of the thump finger tip and index finger tip
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
+
+        # Get the center point of the finger tips
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        # Draw a circle on the x,y coordinates of the finger.
         cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        # Draw a line between the finger tips
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 4)
+
+        # Find the length of the line (tip of both the fingers)
+        length = math.hypot(x2 - x1, y2 - y1)
+        print(f"Length: {length}")
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -33,4 +50,3 @@ while True:
                 (255, 0, 255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
